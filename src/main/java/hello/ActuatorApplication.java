e163ba14a332258ae1f4f72d5a520a97a5b65225
Livelyoneweek <wlvvwhs@naver.com>
2023-03-19 23:29:29 +0900

    모니터링 메트릭 활용 정리

--- src/main/java/hello/ActuatorApplication.java
+++ src/main/java/hello/ActuatorApplication.java
@@ -1,13 +1,17 @@
 package hello;
 
-import hello.order.v0.OrderConfigV0;
-import hello.order.v1.OrderConfigV1;
+import hello.order.gauge.StockConfigV2;
+import hello.order.v4.OrderConfigV4;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 //@Import(OrderConfigV0.class)
-@Import(OrderConfigV1.class)
+//@Import(OrderConfigV1.class)
+//@Import(OrderConfigV2.class)
+//@Import(OrderConfigV3.class)
+//@Import({OrderConfigV4.class, StockConfigV1.class})
+@Import({OrderConfigV4.class, StockConfigV2.class})
 @SpringBootApplication(scanBasePackages = "hello.controller")
 public class ActuatorApplication {
 
---
+++ src/main/java/hello/order/gauge/StockConfigV2.java
@@ -0,0 +1,22 @@
+package hello.order.gauge;
+
+import hello.order.OrderService;
+import io.micrometer.core.instrument.Gauge;
+import io.micrometer.core.instrument.binder.MeterBinder;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+public class StockConfigV2 {
+
+    //Gauge 사용하려면 MeterBinder를 반환하면 됨
+    @Bean
+    public MeterBinder stockSize(OrderService orderService) {
+        return registry -> Gauge.builder("my.stock", orderService, service -> {
+            log.info("stock gauge call");
+            return service.getStock().get();
+        }).register(registry);
+    }
+}
